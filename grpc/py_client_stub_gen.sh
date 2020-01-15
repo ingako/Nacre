24618b85ce7c409a8a1b010998646ad6547fc6b9
ingako <ingako@protonmail.com>
2020-01-16 11:39:45 +1300

    Update configs

--- grpc/py_client_stub_gen.sh
+++ grpc/py_client_stub_gen.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 -m grpc_tools.protoc -I./src/main/proto --python_out=. --grpc_python_out=. seqprediction.proto
+python -m grpc_tools.protoc -I./src/main/proto --python_out=. --grpc_python_out=. seqprediction.proto
