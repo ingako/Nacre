d03f37751c4d1b74ad37a941d779102a0c1b9348
ingako <ingako@protonmail.com>
2020-02-21 10:07:34 +1300

    Add evaluators

---
+++ src/main.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python3.7
+
+import argparse
+import math
+import random
+import pathlib
+import time
+import logging
+import os.path
+
+import numpy as np
+
+from evaluator import Evaluator
+
+import sys
+path = r'../'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+from build.pro_pearl import pearl
+from build.pro_pearl import pro_pearl
+
+formatter = logging.Formatter('%(message)s')
+
+def setup_logger(name, log_file, level=logging.INFO):
+    handler = logging.FileHandler(log_file, mode='w')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cpp",
+                        dest="cpp", action="store_true",
+                        help="Enable cpp backend")
+    parser.set_defaults(cpp=False)
+
+    parser.add_argument("--proactive",
+                        dest="proactive", action="store_true",
+                        help="Enable ProPearl")
+    parser.set_defaults(cpp=False)
+
+    parser.add_argument("--dataset_name",
+                        dest="dataset_name", default="", type=str,
+                        help="dataset path")
+    parser.add_argument("--data_format",
+                        dest="data_format", default="", type=str,
+                        help="dataset format {csv|arff}")
+    parser.add_argument("-g", "--generator",
+                        dest="generator", default="agrawal", type=str,
+                        help="name of the synthetic data generator")
+
+    parser.add_argument("-t", "--tree",
+                        dest="num_trees", default=60, type=int,
+                        help="number of trees in the forest")
+    parser.add_argument("-c", "--candidate_tree",
+                        dest="max_num_candidate_trees", default=60, type=int,
+                        help="max number of candidate trees in the forest")
+    # parser.add_argument("--pool",
+    #                     dest="tree_pool_size", default=180, type=int,
+    #                     help="number of trees in the online tree repository")
+    parser.add_argument("-w", "--warning",
+                        dest="warning_delta", default=0.0001, type=float,
+                        help="delta value for drift warning detector")
+    parser.add_argument("-d", "--drift",
+                        dest="drift_delta", default=0.00001, type=float,
+                        help="delta value for drift detector")
+    parser.add_argument("--max_samples",
+                        dest="max_samples", default=200000, type=int,
+                        help="total number of samples")
+    parser.add_argument("--sample_freq",
+                        dest="sample_freq", default=1000, type=int,
+                        help="log interval for performance")
+    parser.add_argument("--kappa_window",
+                        dest="kappa_window", default=50, type=int,
+                        help="number of instances must be seen for calculating kappa")
+    parser.add_argument("--random_state",
+                        dest="random_state", default=0, type=int,
+                        help="Seed used for adaptive hoeffding tree")
+    parser.add_argument("--generator_seed",
+                        dest="generator_seed", default=0, type=int,
+                        help="Seed used for generating synthetic data")
+    parser.add_argument("--enable_generator_noise",
+                        dest="enable_generator_noise", action="store_true",
+                        help="Enable noise in synthetic data generator")
+    parser.set_defaults(enable_generator_noise=False)
+
+    parser.add_argument("-s", "--enable_state_adaption",
+                        dest="enable_state_adaption", action="store_true",
+                        help="enable the state adaption algorithm")
+    parser.set_defaults(enable_state_adaption=False)
+    parser.add_argument("-p", "--enable_state_graph",
+                        dest="enable_state_graph", action="store_true",
+                        help="enable state transition graph")
+    parser.set_defaults(enable_state_graph=False)
+
+    parser.add_argument("--cd_kappa_threshold",
+                        dest="cd_kappa_threshold", default=0.2, type=float,
+                        help="Kappa value that the candidate tree needs to outperform both"
+                             "background tree and foreground drifted tree")
+    parser.add_argument("--bg_kappa_threshold",
+                        dest="bg_kappa_threshold", default=0.00, type=float,
+                        help="Kappa value that the background tree needs to outperform the "
+                             "foreground drifted tree to prevent from false positive")
+    parser.add_argument("--edit_distance_threshold",
+                        dest="edit_distance_threshold", default=100, type=int,
+                        help="The maximum edit distance threshold")
+    parser.add_argument("--lossy_window_size",
+                        dest="lossy_window_size", default=5, type=int,
+                        help="Window size for lossy count")
+    parser.add_argument("--reuse_window_size",
+                        dest="reuse_window_size", default=0, type=int,
+                        help="Window size for calculating reuse rate")
+    parser.add_argument("--reuse_rate_upper_bound",
+                        dest="reuse_rate_upper_bound", default=0.4, type=float,
+                        help="The reuse rate threshold for switching from "
+                             "pattern matching to graph transition")
+    parser.add_argument("--reuse_rate_lower_bound",
+                        dest="reuse_rate_lower_bound", default=0.1, type=float,
+                        help="The reuse rate threshold for switching from "
+                             "pattern matching to graph transition")
+
+    args = parser.parse_args()
+
+    if args.reuse_rate_upper_bound < args.reuse_rate_lower_bound:
+        exit("reuse rate upper bound must be greater than or equal to the lower bound")
+
+    if args.enable_state_graph:
+        args.enable_state_adaption = True
+
+    potential_file = f"../data/{args.dataset_name}/{args.dataset_name}.{args.data_format}"
+    potential_pre_gen_file = f"../data/{args.generator}/{args.generator}-{args.generator_seed}.csv"
+
+    # prepare data
+    if os.path.isfile(potential_file):
+        print(f"preparing stream from file {potential_file}...")
+
+        if args.cpp:
+            print("speed optimization with C++")
+            stream = potential_file
+        else:
+            stream = FileStream(potential_file)
+            stream.prepare_for_use()
+            args.max_samples = min(args.max_samples, stream.n_remaining_samples())
+
+        result_directory = args.dataset_name
+
+
+    elif os.path.isfile(potential_pre_gen_file):
+        print(f"preparing stream from file {potential_pre_gen_file}...")
+        stream = FileStream(potential_pre_gen_file)
+        stream.prepare_for_use()
+
+        args.max_samples = min(args.max_samples, stream.n_remaining_samples())
+        result_directory = args.generator
+
+    else:
+        print(f"preparing stream from {args.generator} generator...")
+        concepts = [4,0,8]
+        stream = RecurrentDriftStream(generator=args.generator,
+                                      concepts=concepts,
+                                      has_noise=args.enable_generator_noise,
+                                      random_state=args.generator_seed)
+        stream.prepare_for_use()
+        print(stream.get_data_info())
+
+        result_directory = args.generator
+
+    if args.enable_generator_noise:
+        result_directory = f"{result_directory}-noise"
+
+    if args.proactive:
+        result_directory = f"{result_directory}-pro"
+
+    metric_output_file = "result"
+    time_output_file = "time"
+
+    if args.enable_state_graph:
+        result_directory = f"{result_directory}/" \
+                           f"k{args.cd_kappa_threshold}-e{args.edit_distance_threshold}/" \
+                           f"r{args.reuse_rate_upper_bound}-r{args.reuse_rate_lower_bound}-" \
+                           f"w{args.reuse_window_size}/" \
+                           f"lossy-{args.lossy_window_size}"
+
+        metric_output_file = f"{metric_output_file}-parf"
+        time_output_file = f"{time_output_file}-parf"
+
+    elif args.enable_state_adaption:
+        result_directory = f"{result_directory}/" \
+                           f"k{args.cd_kappa_threshold}-e{args.edit_distance_threshold}/"
+
+        metric_output_file = f"{metric_output_file}-sarf"
+        time_output_file = f"{time_output_file}-sarf"
+
+    pathlib.Path(result_directory).mkdir(parents=True, exist_ok=True)
+
+    metric_output_file = f"{result_directory}/{metric_output_file}-{args.generator_seed}.csv"
+    time_output_file = f"{result_directory}/{time_output_file}-{args.generator_seed}.log"
+
+
+    configs = (
+        f"metric_output_file: {metric_output_file}\n"
+        f"warning_delta: {args.warning_delta}\n"
+        f"drift_delta: {args.drift_delta}\n"
+        f"max_samples: {args.max_samples}\n"
+        f"sample_freq: {args.sample_freq}\n"
+        f"kappa_window: {args.kappa_window}\n"
+        f"random_state: {args.random_state}\n"
+        f"enable_state_adaption: {args.enable_state_adaption}\n"
+        f"enable_state_graph: {args.enable_state_graph}\n")
+
+    print(configs)
+    with open(f"{result_directory}/config", 'w') as out:
+        out.write(configs)
+        out.flush()
+
+    if args.cpp:
+        arf_max_features = -1
+        num_features = -1
+    else:
+        num_features = stream.n_features
+        arf_max_features = int(math.log2(num_features)) + 1
+
+    # repo_size = args.num_trees * 160
+    repo_size = args.num_trees * 1600
+    np.random.seed(args.random_state)
+    random.seed(0)
+
+    if args.enable_state_adaption:
+        with open(f"{result_directory}/reuse-rate-{args.generator_seed}.log", 'w') as out:
+            out.write("background_window_count,candidate_window_count,reuse_rate\n")
+
+    metrics_logger = setup_logger('metrics', metric_output_file)
+    process_logger = setup_logger('process', f'{result_directory}/processes-{args.generator_seed}.info')
+
+    if args.cpp:
+        if args.proactive:
+            pearl = pro_pearl(args.num_trees,
+                              args.max_num_candidate_trees,
+                              repo_size,
+                              args.edit_distance_threshold,
+                              args.kappa_window,
+                              args.lossy_window_size,
+                              args.reuse_window_size,
+                              arf_max_features,
+                              args.bg_kappa_threshold,
+                              args.cd_kappa_threshold,
+                              args.reuse_rate_upper_bound,
+                              args.warning_delta,
+                              args.drift_delta)
+            eval_func = Evaluator.prequential_evaluation_proactive
+
+        else:
+            pearl = pearl(args.num_trees,
+                          args.max_num_candidate_trees,
+                          repo_size,
+                          args.edit_distance_threshold,
+                          args.kappa_window,
+                          args.lossy_window_size,
+                          args.reuse_window_size,
+                          arf_max_features,
+                          args.bg_kappa_threshold,
+                          args.cd_kappa_threshold,
+                          args.reuse_rate_upper_bound,
+                          args.warning_delta,
+                          args.drift_delta,
+                          args.enable_state_adaption,
+                          args.enable_state_graph)
+            eval_func = Evaluator.prequential_evaluation_cpp
+
+    else:
+        print("Non-CPP version is unsupported")
+        exit()
+
+    start = time.process_time()
+    eval_func(classifier=pearl,
+              stream=stream,
+              max_samples=args.max_samples,
+              sample_freq=args.sample_freq,
+              metrics_logger=metrics_logger)
+    elapsed = time.process_time() - start
+
+    with open(f"{time_output_file}", 'w') as out:
+        out.write(str(elapsed) + '\n')
