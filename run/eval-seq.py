9f311d2d2f53ae3d556a83ce32e596bd25b46d6e
ingako <ingako@protonmail.com>
2020-06-05 20:05:25 +1200

    Calculate precision & call, add a new method for calculating TP & FP, add best params

--- run/eval-seq.py
+++ run/eval-seq.py
@@ -24,6 +24,7 @@ def is_empty_file(fpath):
     return False if os.path.isfile(fpath) and os.path.getsize(fpath) > 0 else True
 
 
+# ./eval-seq.py agrawal/abrupt/poisson10 600
 base_dir = os.getcwd()
 generator = sys.argv[1]
 end = int(sys.argv[2]) * 1000
@@ -31,21 +32,47 @@ end = int(sys.argv[2]) * 1000
 data_dir = f"../data/{generator}/"
 
 param_strs = ["seq", "backtrack", "adapt_window", "stability", "hybrid"]
+
 agrawal_abrupt_poisson10_params = ['8', '25', '200', '0.01', '0.9']
-nacre_param_path = "/".join(agrawal_abrupt_poisson10_params)
+agrawal_abrupt_poisson3_params = ['8', '25', '400', '0.001', '0.9']
+agrawal_abrupt_uniform_params = ['8', '25', '500', '0.001', '0.9']
+
+agrawal_gradual_poisson10_params = ['8', '25', '400', '0.01', '0.9']
+agrawal_gradual_poisson3_params = ['8', '25', '300', '0.1', '0.9']
+agrawal_gradual_uniform_params = ['8', '25', '400', '0.1', '0.9']
+
+
+# nacre_param_path = "/".join(agrawal_abrupt_poisson10_params)
+nacre_param_path = "/".join(agrawal_abrupt_poisson3_params)
+#nacre_param_path = "/".join(agrawal_abrupt_uniform_params)
+
+
+agrawal_params = Param(
+        generator = generator,
+        kappa=0.1,
+        ed=100,
+        reuse_window_size=0,
+        reuse_rate=0.9,
+        lossy_window_size=100000000,
+        poisson_lambda=6,
+        kappa_window=50)
+
+tree_params = Param(
+        generator = generator,
+        kappa=0.0,
+        ed=100,
+        reuse_window_size=0,
+        reuse_rate=0.9,
+        lossy_window_size=100000000,
+        poisson_lambda=6,
+        kappa_window=50)
+
+if generator[:7] == "agrawal":
+    p = agrawal_params
+elif generator[:4] == "tree":
+    p = tree_params
 
 for seed in range(0, 10):
-    agrawal_params = Param(
-            generator = generator,
-            seed = seed,
-            kappa=0.1,
-            ed=100,
-            reuse_window_size=0,
-            reuse_rate=0.9,
-            lossy_window_size=100000000,
-            poisson_lambda=6,
-            kappa_window=50)
-    p = agrawal_params
 
     drift_log_path = f"{data_dir}/drift-{seed}.log"
     arf_data_dir = f"{base_dir}/{generator}"
@@ -61,7 +88,10 @@ for seed in range(0, 10):
     all_predictions = []
     with open(all_predictions_path) as f:
         for line in f:
-            all_predictions.append([int(v) for v in line.split(",")])
+            if line == "\n":
+                all_predictions.append([])
+            else:
+                all_predictions.append([int(v) for v in line.split(",")])
 
     actual_drifts = []
     with open(drift_log_path) as f:
@@ -70,11 +100,12 @@ for seed in range(0, 10):
             if int(line) > end:
                 break
 
-    offset = 200
     for tree_idx in range(60):
         all_prediction = all_predictions[tree_idx]
         i, j = 0, 0
         true_positive, false_positive = 0, 0
+
+        offset = 1000
         while i < len(all_prediction) and j < len(actual_drifts):
             if all_prediction[i] < actual_drifts[j]:
                 if all_prediction[i] <= actual_drifts[j] - offset:
@@ -90,4 +121,26 @@ for seed in range(0, 10):
                     true_positive += 1
                     i += 1
                 j += 1
-        print(f"true_positive={true_positive}, false_positive={false_positive}")
+
+        # offset = 1000
+        # while i < len(all_prediction) and j < len(actual_drifts):
+        #     if all_prediction[i] < actual_drifts[j]:
+        #         false_positive += 1
+        #         i += 1
+        #     else:
+        #         if all_prediction[i] >= actual_drifts[j] + offset:
+        #             false_positive += 1
+        #         else:
+        #             true_positive += 1
+        #             i += 1
+        #         j += 1
+
+        false_negative  = len(actual_drifts) - true_positive
+        print(f"tree {tree_idx} : true_positive={true_positive}, false_positive={false_positive}, false_negative={false_negative}")
+
+        if true_positive + false_positive == 0 or true_positive + false_negative == 0:
+            print(f"precision=0 recall=0")
+            continue
+        precision = true_positive / (true_positive + false_positive)
+        recall = true_positive / (true_positive + false_negative)
+        print(f"precision={precision}, recall={recall}")
